8ead123bdf05e95b72704ce25799515115527b1b
KloversFaine <ltl0124@163.com>
2019-05-16 14:29:25 +0800

    Update dp_coin.java

--- dp_coin.java
+++ dp_coin.java
@@ -25,33 +25,48 @@ public class DP_coin {
 		switch(option) 
 		{
 			case 1:
-				res = normal_coin(count);	//方案一	
+				res = normal_coin(count); //方案一	
+				if(!check_res(res))
 				System.out.println("method_01_res:"+ res);
 				break;
 			case 2:
 				res = dpdesign_coin(count); //方案二
+				if(!check_res(res))
 				System.out.println("method_02_res:"+ res);
 				break;
 			case 3:
 				res = common_dp_coin(count); //方案三
+				if(!check_res(res))
 				System.out.println("method_03_res:"+ res);
 				break;
 			case 4:
 				res = single_dp_coin(count); //方案四
+				if(!check_res(res))
 				System.out.println("method_04_res:"+ res);
 				break;
 			case 5:
 				res = single_common_dp_coin(count); //方案五
+				if(!check_res(res))
 				System.out.println("method_05_res:"+ res);
 				break;
 			default :
 				System.out.println("输入选项不正确，本次服务到此结束");
 		}
 	}
-
+	
+	//针对res的check_res函数
+	public static boolean check_res(int res) {
+		if(res != MAX_NUM) {
+			return false;
+		}else {
+			System.out.println("无法给出合适的找零方案");
+			return true;
+		}			
+	}
+	
 	//method_01 : normal_coin() 
 	//针对题设的1，3，5的情况，贪心算法可解	
-	public static int normal_coin(int count) {
+ 	public static int normal_coin(int count) {
 		int res = 0;
 		res = count/5;
 		count= count%5;
@@ -147,12 +162,73 @@ public class DP_coin {
 	
 	//method_04 : single_dp_coin
 	public static int single_dp_coin(int count) {
-		return 0;
+		int arr[] = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		int dp[][] = new int[arr.length][count+1];
+		for(int i=0;i<arr.length;i++) {
+			dp[i][0] = 0;
+		}
+		for(int j=0;j<count+1;j++) {
+			if(j==arr[0]) {
+				dp[0][j] = 1;
+			}else {
+				dp[0][j] = MAX_NUM;
+			}
+		}
+		//update the Dim-2 array
+		for(int i=1;i<arr.length;i++) {
+			for(int j=1;j<count+1;j++) {
+				if(arr[i]>j) {
+					dp[i][j] = dp[i-1][j];
+				}else {
+					dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j-arr[i]]+1);
+					//使用dp[i-1][j-arr[i]]+1的原因：每种面额的硬币只有一张
+				}
+			}
+		}
+		int res = dp[arr.length-1][count];
+		return res;
 	}
 	
-	//method_05 : single_common_dp_coin
+	//method_05 : single_common_dp_coin(need to update)
+	//再次明确dp[i][j]的含义：找零j的钱数，用i面额的钱的情况下一共需要几张
 	public static int single_common_dp_coin(int count) {	
-		return 0;
+		Scanner scanner = new Scanner(System.in);
+		//输入可选择的硬币种类
+		System.out.println("请输入可选择的硬币种类");
+		int m = scanner.nextInt();
+		int arr[] = new int[m];
+		System.out.println("请依次输入硬币面值");
+		//输入并初始化arr[] 
+		//TODO --> error check : 输入的硬币面值应当各不相同
+		for(int i=0;i<m;i++) {
+			arr[i]=scanner.nextInt();
+		}
+		//新建dp数组
+		int dp[][] = new int[m][count+1];
+		//初始化
+		for(int i=0;i<m;i++) {
+			dp[i][0] = 0;
+		}
+		for(int j=0;j<count+1;j++) {
+			if(j==arr[0]) {
+				dp[0][j] = 1;
+			}else {
+				dp[0][j] = MAX_NUM;
+			}
+		}
+		//update the Dim-2 array
+		for(int i=1;i<arr.length;i++) {
+			for(int j=1;j<count+1;j++) {
+				if(arr[i]>j) {
+					dp[i][j] = dp[i-1][j];
+				}else {
+					dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j-arr[i]]+1);
+					//使用dp[i-1][j-arr[i]]+1的原因：每种面额的硬币只有一张
+				}
+			}
+		}
+		int res = dp[m-1][count];
+		return res;
 	}
 }
 
